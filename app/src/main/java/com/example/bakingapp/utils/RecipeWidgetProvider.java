87aaf93b5c46530c5318bcfa95dfe9c1a8797b7f
abdullah10182 <abdullah10182@gmail.com>
2019-11-06 12:25:22 -0400

    test2

--- app/src/main/java/com/example/bakingapp/utils/RecipeWidgetProvider.java
+++ app/src/main/java/com/example/bakingapp/utils/RecipeWidgetProvider.java
@@ -1,9 +1,15 @@
-package com.example.bakingapp;
+package com.example.bakingapp.utils;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.example.bakingapp.MainActivity;
+import com.example.bakingapp.R;
+import com.example.bakingapp.activities.RecipeDetailActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -13,10 +19,13 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget_provider);
-        //views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        Intent intent = new Intent(context, RecipeDetailActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        views.setOnClickPendingIntent(R.id.iv_widget_cupcake_image, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
